ac9b9569ea49ddeb5cc0c183001aa0931e247dbc
Elliott Mejia <mejia.elliott@gmail.com>
2024-04-02 04:32:52 -0700

    polka dots!

--- src/components/taskbar/index.tsx
+++ src/components/taskbar/index.tsx
@@ -26,30 +26,38 @@ const Taskbar = ({ handlePrint }: Props) => {
   const triggerClasses =
     "px-4 py-2  hover:shadow-pressed rounded-none animate ease-in-out duration-100";
   return (
-    <Menubar className="rounded-none relative top-2 -right-8 p-0 w-fit ">
-      <MenubarMenu>
-        <MenubarTrigger className={triggerClasses}>Share</MenubarTrigger>
-        <MenubarContent>
-          {/* <MenubarItem disabled>New Incognito Window</MenubarItem> */}
-          <MenubarItem onClick={() => clipboardCopy(HOSTED_DOMAIN)}>
-            <Icon icon="material-symbols:link" />
-            Copy Link
-          </MenubarItem>
-          <MenubarItem>Email Pdf</MenubarItem>
-        </MenubarContent>
-      </MenubarMenu>
-      <MenubarMenu>
-        <MenubarTrigger className={triggerClasses} onClick={handlePrint}>
-          Print
-        </MenubarTrigger>
-      </MenubarMenu>
-      <MenubarMenu>
-        <MenubarTrigger className={triggerClasses} onClick={handlePDF}>
-          PDF
-        </MenubarTrigger>
-      </MenubarMenu>
-      <div className="hidden px-4 py-2 hover:bg-gray-200 hover:border-radius-pressed "></div>
-    </Menubar>
+    <>
+      <Menubar
+        id="taskbar"
+        className="rounded-none relative  top-2 clear right-2 p-0 w-fit mx-auto"
+        style={{
+          clear: "both",
+        }}
+      >
+        <MenubarMenu>
+          <MenubarTrigger className={triggerClasses}>Share</MenubarTrigger>
+          <MenubarContent>
+            {/* <MenubarItem disabled>New Incognito Window</MenubarItem> */}
+            <MenubarItem onClick={() => clipboardCopy(HOSTED_DOMAIN)}>
+              <Icon icon="material-symbols:link" />
+              Copy Link
+            </MenubarItem>
+            <MenubarItem>Email Pdf</MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+        <MenubarMenu>
+          <MenubarTrigger className={triggerClasses} onClick={handlePrint}>
+            Print
+          </MenubarTrigger>
+        </MenubarMenu>
+        <MenubarMenu>
+          <MenubarTrigger className={triggerClasses} onClick={handlePDF}>
+            PDF
+          </MenubarTrigger>
+        </MenubarMenu>
+        <div className="hidden px-4 py-2 hover:bg-gray-200 hover:border-radius-pressed "></div>
+      </Menubar>
+    </>
   );
 };
 
